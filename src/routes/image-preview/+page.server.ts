6a81a63ce41f5b7e05fc9b21637fb7eb82c1e283
Ahmad Ferdian <ahmadferdian@Ahmads-MacBook-Air.local>
2024-01-07 18:16:11 +0700

    Refactor: Update image from import

--- src/routes/image-preview/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async () => {
-	const images: String[] = ['cat-1.jpg', 'cat-2.jpg', 'cat-3.jpg'];
-
-	return {
-		images
-	};
-};
--- src/routes/image-preview/+page.svelte
+++ src/routes/image-preview/+page.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
-	import type { PageServerData } from './$types';
+	import cat1 from '$lib/assets/image-preview/images/cat-1.jpg';
+	import cat2 from '$lib/assets/image-preview/images/cat-2.jpg';
+	import cat3 from '$lib/assets/image-preview/images/cat-3.jpg';
 
-	export let data: PageServerData;
+	const images = [cat1, cat2, cat3];
 </script>
 
 <div class="flex flex-col justify-center items-center h-[calc(100dvh)]">
 	<div class="card card-bordered w-64 shadow-xl">
 		<div class="card-body justify-evenly">
 			<div class="card-title"><h1>Image Preview</h1></div>
-			{#each data.images as img}
-				<a href="src/lib/assets/image-preview/images/{img}" target="_blank" class="group">
+			{#each images as img}
+				<a href={img} target="_blank" class="group">
 					<img
-						src="src/lib/assets/image-preview/images/{img}"
-						alt={img.toString()}
+						src={img}
+						alt={img}
 						class="w-full h-40 object-cover rounded transform transition group-hover:translate-x-1"
 					/>
 				</a>
